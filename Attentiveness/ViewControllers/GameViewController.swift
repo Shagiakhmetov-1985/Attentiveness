8e9fbf7ac5d01603fba3b18af5567efbbd472604
Marat <Shagiakhmetov2499@gmail.com>
2024-02-05 19:05:21 +0300

    Added label time for start game after 3 seconds and added buttons "Yes" and "No" for start a new game or exit game if user managed to find everything. Added animations of seconds for start game. For buttons "Yes" and "No" added stack view.

--- Attentiveness/ViewControllers/GameViewController.swift
+++ Attentiveness/ViewControllers/GameViewController.swift
@@ -12,6 +12,34 @@ protocol GameViewControllerDelegate {
 }
 
 class GameViewController: UIViewController {
+    private lazy var labelTime: UILabel = {
+        setupLabel(title: "\(seconds)", size: 150, opacity: 1)
+    }()
+    
+    private lazy var buttonYes: UIButton = {
+        setupButton(
+            title: "Yes",
+            color: .systemGreen,
+            action: #selector(pressYes),
+            isEnabled: true)
+    }()
+    
+    private lazy var buttonNo: UIButton = {
+        setupButton(
+            title: "No",
+            color: .systemRed,
+            action: #selector(pressNo),
+            isEnabled: true)
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [buttonYes, buttonNo])
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     private lazy var buttonClose: UIButton = {
         let size = UIImage.SymbolConfiguration(pointSize: 30)
         let image = UIImage(systemName: "multiply", withConfiguration: size)
@@ -21,6 +49,7 @@ class GameViewController: UIViewController {
         button.layer.shadowOpacity = 0.4
         button.layer.shadowOffset = CGSize(width: 0, height: 4)
         button.layer.shadowRadius = 4
+        button.layer.opacity = 0
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(backToMenu), for: .touchUpInside)
         return button
@@ -33,6 +62,7 @@ class GameViewController: UIViewController {
     private lazy var imageTitle: UIImageView = {
         let imageView = UIImageView()
         imageView.tintColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        imageView.layer.opacity = 0
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -51,24 +81,17 @@ class GameViewController: UIViewController {
         collectionView.register(ImagesCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.layer.opacity = 0
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
     
     private lazy var buttonReset: UIButton = {
-        let button = UIButton(type: .system)
-        button.titleLabel?.font = UIFont(name: "copperplate", size: 25)
-        button.setTitle("RESET", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemGray2
-        button.layer.cornerRadius = 8
-        button.layer.shadowOpacity = 0.4
-        button.layer.shadowOffset = CGSize(width: 0, height: 4)
-        button.layer.shadowRadius = 4
-        button.isEnabled = false
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(reset), for: .touchUpInside)
-        return button
+        setupButton(
+            title: "RESET",
+            color: .systemGray2,
+            action: #selector(reset),
+            isEnabled: false)
     }()
     
     var images: ([String], [String])!
@@ -78,6 +101,8 @@ class GameViewController: UIViewController {
     private var imageTap = String()
     private var isHidden: [Bool] = []
     private var countTap = 0
+    private var timer = Timer()
+    private var seconds = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -87,21 +112,31 @@ class GameViewController: UIViewController {
         setRandomImage()
         setHidden()
         setupConstraints()
+        startGame()
     }
     
     private func setupDesign() {
         view.backgroundColor = #colorLiteral(red: 0.8025280833, green: 0.9857317805, blue: 0.6650850177, alpha: 1)
         navigationItem.hidesBackButton = true
+        hideTime()
     }
     
     private func setupSubviews() {
-        setupSubviews(subviews: collectionView, labelTitle, imageTitle,
-                      labelName, buttonReset, on: view)
+        setupSubviews(subviews: labelTime, stackView, collectionView, labelTitle,
+                      imageTitle, labelName, buttonReset, on: view)
     }
     
     private func setupSubviews(subviews: UIView..., on otherSubview: UIView) {
         subviews.forEach { subview in
             otherSubview.addSubview(subview)
+        }
+    }
+    
+    private func opacityOnOff(subviews: UIView..., opacity: Float) {
+        UIView.animate(withDuration: 0.6) {
+            subviews.forEach { subview in
+                subview.layer.opacity = opacity
+            }
         }
     }
     
@@ -111,7 +146,7 @@ class GameViewController: UIViewController {
     }
     
     private func setRandomImage() {
-        guard !images.1.isEmpty else { return }
+        guard !images.1.isEmpty else { return congratulations() }
         let index = Int.random(in: 0...images.1.count - 1)
         let imageName = images.1[index]
         setPicture(image: imageName)
@@ -126,6 +161,17 @@ class GameViewController: UIViewController {
         labelName.text = "\(text.capitalized)"
     }
     
+    private func congratulations() {
+        subviewsOnOff(isOn: false)
+        setTitleTimer(time: """
+                      Congratulations!
+                      You managed to find everything.
+                      Would you like start a new game?
+                      """)
+        labelTime.font = UIFont(name: "copperplate", size: 32)
+        opacityOnOff(subviews: buttonYes, buttonNo, opacity: 1)
+    }
+    
     private func setHidden() {
         images.0.forEach { _ in
             isHidden.append(false)
@@ -135,6 +181,51 @@ class GameViewController: UIViewController {
     private func resetIsHidden() {
         isHidden.removeAll()
         setHidden()
+    }
+    
+    private func runTimer(time: CGFloat, action: Selector, repeats: Bool) -> Timer {
+        Timer.scheduledTimer(timeInterval: time, target: self, selector: action,
+                             userInfo: nil, repeats: repeats)
+    }
+    
+    private func startGame() {
+        showTime(time: 1, scale: 1.5)
+        timer = runTimer(time: 1, action: #selector(runTime), repeats: true)
+    }
+    
+    @objc private func runTime() {
+        seconds -= 1
+        hideTime()
+        seconds > 0 ? showTime(time: 1, scale: 1.5) : showTime(time: 0.3, scale: 1)
+        seconds > 0 ? setTitleTimer(time: "\(seconds)") : setTitleTimer(time: "GO!")
+        guard seconds == 0 else { return }
+        timer.invalidate()
+        timer = runTimer(time: 0.6, action: #selector(showSubviews), repeats: false)
+    }
+    
+    private func showTime(time: CGFloat, scale: CGFloat) {
+        UIView.animate(withDuration: time) {
+            self.labelTime.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }
+    }
+    
+    private func hideTime() {
+        labelTime.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    }
+    
+    private func setTitleTimer(time: String) {
+        labelTime.text = time
+    }
+    
+    @objc private func showSubviews() {
+        timer.invalidate()
+        subviewsOnOff(isOn: true)
+    }
+    
+    private func subviewsOnOff(isOn: Bool) {
+        opacityOnOff(subviews: buttonClose, labelTitle, imageTitle, labelName,
+                     collectionView, buttonReset, opacity: isOn ? 1 : 0)
+        opacityOnOff(subviews: labelTime, opacity: isOn ? 0 : 1)
     }
     
     @objc private func backToMenu() {
@@ -157,6 +248,22 @@ class GameViewController: UIViewController {
         default: Images.getSport(count: count)
         }
     }
+    
+    @objc private func pressYes() {
+        opacityOnOff(subviews: labelTime, buttonYes, buttonNo, opacity: 0)
+        reset()
+        timer = runTimer(time: 0.6, action: #selector(startNewGame), repeats: false)
+    }
+    
+    @objc private func startNewGame() {
+        timer.invalidate()
+        opacityOnOff(subviews: buttonClose, labelTitle, imageTitle, labelName,
+                     collectionView, buttonReset, opacity: 1)
+    }
+    
+    @objc private func pressNo() {
+        navigationController?.popViewController(animated: false)
+    }
 }
 
 extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -175,18 +282,54 @@ extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSour
 }
 
 extension GameViewController {
-    private func setupLabel(title: String, size: CGFloat) -> UILabel {
+    private func setupLabel(title: String, size: CGFloat, opacity: Float? = nil) -> UILabel {
         let label = UILabel()
         label.font = UIFont(name: "copperplate", size: size)
         label.text = title
         label.textColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        label.layer.opacity = opacity ?? 0
+        label.textAlignment = .center
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }
 }
 
 extension GameViewController {
+    private func setupButton(title: String, color: UIColor, action: Selector, 
+                             isEnabled: Bool) -> UIButton {
+        let button = UIButton(type: .system)
+        button.titleLabel?.font = UIFont(name: "copperplate", size: 25)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = color
+        button.layer.cornerRadius = 8
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 4
+        button.isEnabled = isEnabled
+        button.layer.opacity = 0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: action, for: .touchUpInside)
+        return button
+    }
+}
+
+extension GameViewController {
     private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            labelTime.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelTime.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            labelTime.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            labelTime.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: labelTime.bottomAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5)
+        ])
+        
         NSLayoutConstraint.activate([
             collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
