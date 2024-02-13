7193765f47c5340cdfa494d3b822d29ad9136b09
Marat <Shagiakhmetov2499@gmail.com>
2024-02-13 20:35:31 +0300

    Added timer label and circle timer for countdown. Optimized code and deleted extra codes. Added animation of collection view for tap wrong icons and added method for stack view subview.

--- Attentiveness/ViewControllers/GameViewController.swift
+++ Attentiveness/ViewControllers/GameViewController.swift
@@ -39,11 +39,7 @@ class GameViewController: UIViewController {
     }()
     
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [buttonYes, buttonNo])
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
+        setupStackView(spacing: 10, first: buttonYes, second: buttonNo)
     }()
     
     private lazy var imageFirst: UIImageView = {
@@ -56,6 +52,10 @@ class GameViewController: UIViewController {
     
     private lazy var imageThird: UIImageView = {
         setupImage(image: "heart.fill")
+    }()
+    
+    private lazy var stackViewImages: UIStackView = {
+        setupStackView(spacing: -5, first: imageFirst, second: imageSecond, third: imageThird)
     }()
     
     private lazy var buttonClose: UIButton = {
@@ -71,6 +71,10 @@ class GameViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(backToMenu), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var labelCountdown: UILabel = {
+        setupLabel(title: "", size: 35)
     }()
     
     private lazy var labelTitle: UILabel = {
@@ -129,6 +133,8 @@ class GameViewController: UIViewController {
     private var timer = Timer()
     private var seconds = 3
     private var wrong = 0
+    private var springCollectionView: NSLayoutConstraint!
+    private let shapeLayer = CAShapeLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -148,7 +154,7 @@ class GameViewController: UIViewController {
     }
     
     private func setupSubviews() {
-        setupSubviews(subviews: imageFirst, imageSecond, imageThird, labelTime,
+        setupSubviews(subviews: stackViewImages, labelCountdown, labelTime,
                       stackView, collectionView, labelTitle, imageTitle,
                       labelName, buttonReset, on: view)
     }
@@ -175,8 +181,8 @@ class GameViewController: UIViewController {
     
     private func darkScreenOnOff(isOn: Bool) {
         let opacity: Float = isOn ? 0.4 : 1
-        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, buttonClose,
-                     labelTitle, imageTitle, labelName, collectionView,
+        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, labelCountdown,
+                     buttonClose, labelTitle, imageTitle, labelName, collectionView,
                      buttonReset, opacity: opacity)
         enableOnOff(subviews: buttonClose, collectionView, buttonReset, isOn: !isOn)
     }
@@ -203,10 +209,11 @@ class GameViewController: UIViewController {
     }
     
     private func endGame() {
+        timer.invalidate()
+        seconds > 0 ? stopAnimationCircleCountdown() : ()
         let title = wrong < 3 ? titleVictory() : titleDefeat()
         subviewsOnOff(isOn: false)
-        setTitleTimer(time: "\(title)")
-        labelTime.font = UIFont(name: "copperplate", size: 32)
+        setTitle(labelTime, "\(title)", 32)
         opacityOnOff(subviews: buttonYes, buttonNo, opacity: 1)
     }
     
@@ -242,8 +249,7 @@ class GameViewController: UIViewController {
     }
     
     private func startGame() {
-        labelTime.font = UIFont(name: "copperplate", size: 150)
-        setTitleTimer(time: "\(seconds)")
+        setTitle(labelTime, "\(seconds)", 150)
         showTime(time: 1, scale: 1.5)
         timer = runTimer(time: 1, action: #selector(runTime), repeats: true)
     }
@@ -252,7 +258,7 @@ class GameViewController: UIViewController {
         seconds -= 1
         hideTime()
         seconds > 0 ? showTime(time: 1, scale: 1.5) : showTime(time: 0.3, scale: 1)
-        seconds > 0 ? setTitleTimer(time: "\(seconds)") : setTitleTimer(time: "GO!")
+        seconds > 0 ? setTitle(labelTime, "\(seconds)", 150) : setTitle(labelTime, "GO!", 150)
         guard seconds == 0 else { return }
         timer.invalidate()
         timer = runTimer(time: 0.6, action: #selector(showSubviews), repeats: false)
@@ -268,23 +274,55 @@ class GameViewController: UIViewController {
         labelTime.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
     }
     
-    private func setTitleTimer(time: String) {
-        labelTime.text = time
+    private func setTitle(_ label: UILabel, _ title: String, _ size: CGFloat) {
+        label.text = title
+        label.font = UIFont(name: "copperplate", size: size)
     }
     
     @objc private func showSubviews() {
         timer.invalidate()
         subviewsOnOff(isOn: true)
+        setCircleCountdown()
+        setTitleCountdown()
+        timer = runTimer(time: 0.6, action: #selector(runCountdown), repeats: false)
+    }
+    
+    private func setCircleCountdown() {
+        circularShadow()
+        circular(strokeEnd: 0)
+        animationCircleCountdownReset()
+    }
+    
+    private func setTitleCountdown() {
+        seconds = timeCoundown(count: count)
+        labelCountdown.text = "\(seconds)"
     }
     
     private func subviewsOnOff(isOn: Bool) {
-        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, buttonClose,
-                     labelTitle, imageTitle, labelName, collectionView,
+        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, labelCountdown,
+                     buttonClose, labelTitle, imageTitle, labelName, collectionView,
                      buttonReset, opacity: isOn ? 1 : 0)
         opacityOnOff(subviews: labelTime, opacity: isOn ? 0 : 1)
     }
     
+    @objc private func runCountdown() {
+        timer.invalidate()
+        shapeLayer.strokeEnd = 1
+        animationRunCircleCountdown()
+        timer = runTimer(time: 1, action: #selector(setTitleTime), repeats: true)
+    }
+    
+    @objc private func setTitleTime() {
+        seconds -= 1
+        labelCountdown.text = "\(seconds)"
+        guard seconds == 0 else { return }
+        timer.invalidate()
+        wrong = 3
+        endGame()
+    }
+    
     @objc private func backToMenu() {
+        timer.invalidate()
         navigationController?.popViewController(animated: false)
     }
     
@@ -329,9 +367,13 @@ class GameViewController: UIViewController {
     
     @objc private func startNewGame() {
         timer.invalidate()
-        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, buttonClose,
-                     labelTitle, imageTitle, labelName, collectionView,
+        opacityOnOff(subviews: imageFirst, imageSecond, imageThird, labelCountdown,
+                     buttonClose, labelTitle, imageTitle, labelName, collectionView,
                      buttonReset, opacity: 1)
+        shapeLayer.strokeEnd = 0
+        animationCircleCountdownReset()
+        setTitleCountdown()
+        timer = runTimer(time: 0.6, action: #selector(runCountdown), repeats: false)
     }
     
     @objc private func pressNo() {
@@ -389,6 +431,20 @@ extension GameViewController {
 }
 
 extension GameViewController {
+    private func setupStackView(spacing: CGFloat, first: UIView, second: UIView, 
+                                third: UIView? = nil) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: [first, second])
+        stackView.spacing = spacing
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        if let third = third {
+            stackView.addArrangedSubview(third)
+        }
+        return stackView
+    }
+}
+
+extension GameViewController {
     private func setupImage(image: String) -> UIImageView {
         let size = UIImage.SymbolConfiguration(pointSize: 28)
         let image = UIImage(systemName: image, withConfiguration: size)
@@ -403,18 +459,13 @@ extension GameViewController {
 extension GameViewController {
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            imageFirst.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -37.5),
-            imageFirst.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
+            stackViewImages.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -37.5),
+            stackViewImages.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
         ])
         
         NSLayoutConstraint.activate([
-            imageSecond.centerYAnchor.constraint(equalTo: imageFirst.centerYAnchor),
-            imageSecond.leadingAnchor.constraint(equalTo: imageFirst.trailingAnchor, constant: -5)
-        ])
-        
-        NSLayoutConstraint.activate([
-            imageThird.centerYAnchor.constraint(equalTo: imageSecond.centerYAnchor),
-            imageThird.leadingAnchor.constraint(equalTo: imageSecond.trailingAnchor, constant: -5)
+            labelCountdown.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelCountdown.topAnchor.constraint(equalTo: view.topAnchor, constant: 80)
         ])
         
         NSLayoutConstraint.activate([
@@ -430,8 +481,11 @@ extension GameViewController {
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5)
         ])
         
+        springCollectionView = NSLayoutConstraint(
+            item: collectionView, attribute: .centerX, relatedBy: .equal,
+            toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+        view.addConstraint(springCollectionView)
         NSLayoutConstraint.activate([
-            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
             collectionView.widthAnchor.constraint(equalToConstant: 325),
             collectionView.heightAnchor.constraint(equalToConstant: height(count: count))
@@ -478,6 +532,82 @@ extension GameViewController {
     }
 }
 
+extension GameViewController {
+    private func circularShadow() {
+        let center = CGPoint(x: labelCountdown.center.x, y: labelCountdown.center.y + 17.5)
+        let endAngle = CGFloat.pi / 2
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let circularPath = UIBezierPath(
+            arcCenter: center,
+            radius: 33,
+            startAngle: -startAngle,
+            endAngle: -endAngle,
+            clockwise: true)
+        
+        let trackShape = CAShapeLayer()
+        trackShape.path = circularPath.cgPath
+        trackShape.lineWidth = 8
+        trackShape.fillColor = UIColor.clear.cgColor
+        trackShape.strokeColor = UIColor.white.withAlphaComponent(0.3).cgColor
+        view.layer.addSublayer(trackShape)
+    }
+    
+    private func circular(strokeEnd: CGFloat) {
+        let center = CGPoint(x: labelCountdown.center.x, y: labelCountdown.center.y + 17.5)
+        let endAngle = CGFloat.pi / 2
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let circularPath = UIBezierPath(
+            arcCenter: center,
+            radius: 33,
+            startAngle: -startAngle,
+            endAngle: -endAngle,
+            clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.lineWidth = 8
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = strokeEnd
+        shapeLayer.lineCap = CAShapeLayerLineCap.butt
+        shapeLayer.strokeColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1).cgColor
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animationRunCircleCountdown() {
+        let timer = timeCoundown(count: count)
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 0
+        animation.duration = CFTimeInterval(timer)
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        shapeLayer.add(animation, forKey: "animation")
+    }
+    
+    private func animationCircleCountdownReset() {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 1
+        animation.duration = CFTimeInterval(0.4)
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        shapeLayer.add(animation, forKey: "animation")
+    }
+    
+    private func stopAnimationCircleCountdown() {
+        let fullTime = timeCoundown(count: count)
+        let time = CGFloat(seconds) / CGFloat(fullTime)
+        let result = round(100 * time) / 100
+        shapeLayer.removeAnimation(forKey: "animation")
+        shapeLayer.strokeEnd = result
+    }
+    
+    private func timeCoundown(count: Count) -> Int {
+        switch count {
+        case .twelve: return 20
+        case .twentyFour: return 40
+        default: return 60
+        }
+    }
+}
+
 extension GameViewController: GameViewControllerDelegate {
     func didTapCell(image: String, index: Int) {
         isHidden[index] = true
@@ -504,12 +634,26 @@ extension GameViewController: GameViewControllerDelegate {
 extension GameViewController {
     func didWrondTapCell() {
         wrong += 1
+        setAnimation()
         switch wrong {
         case 1: opacityOnOff(subviews: imageThird, opacity: 0)
         case 2: opacityOnOff(subviews: imageSecond, opacity: 0)
         default: 
             opacityOnOff(subviews: imageFirst, opacity: 0)
             endGame()
+        }
+    }
+    
+    private func setAnimation() {
+        animation(duration: 0.07, delay: 0, constant: 40)
+        animation(duration: 0.07, delay: 0.07, constant: -80)
+        animation(duration: 0.07, delay: 0.14, constant: 40)
+    }
+    
+    private func animation(duration: CGFloat, delay: CGFloat, constant: CGFloat) {
+        UIView.animate(withDuration: duration, delay: delay) { [self] in
+            springCollectionView.constant += constant
+            view.layoutIfNeeded()
         }
     }
 }
